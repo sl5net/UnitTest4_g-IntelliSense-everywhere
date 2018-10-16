acac55c4ae7ee238d044a48cd0d99d9fab850540
Sebastian <sl5softwarelab@gmail.com>
2018-10-16 13:48:28 +0200

    work: read list of ahk files

--- src/parse_AHK_helpfile.kt
+++ src/parse_AHK_helpfile.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-fun info(sFileStr: String) {
+fun infoWindowsOS_File(sFileStr: String) {
     val file = File(sFileStr)
     print("/¯¯¯¯¯¯¯ ")
     print("isDir = " + file.isDirectory() + ", ")
@@ -26,60 +26,14 @@ fun info(sFileStr: String) {
     println("\\_______")
 }
 fun main(args: Array<String>) {
-    info("""C:""")
-    info("""C:\Windows""")
-    info(""""G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\""")
-    info(""""G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\""")
-    info(""""G:\downloads""")
-    info(""""g:\downloads""")
-    info(""""g:""")
+//    doSomeTests()
+    File("""G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\""").walk().forEach { println(it) }
+}
 
-/*    var sRaw = """"G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\"""
-    var sRaw = """"G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\"""
-    println("rawPathStr= " + sRaw)
-
-    var preparedPathWithoutDocFolderStr_1 = """"G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\"""
-            .replace("\\", "\\\\")
-    println("preparedPathStr =" + preparedPathStr)
-
-    var preparedWithoutDocFolderStr = rawPathStr.replace("\\", "\\\\")
-    println("s2= " + sWithoutDocs)
-//    val replace = s.replace("\\", File.separator)
-
-    val rawPathStrFolder1 = File(rawPathStr)
-    val folder2 = File(sWithoutDocs)
-//¯¯¯¯ not working: s is dir? false
-    println("rawPathStrFolder1 is dir? " + rawPathStrFolder1.isDirectory())
-    println("folder2 is dir? " + folder2.isDirectory())
-//    s is dir? false
-//    s2 is dir? false
-//\____ not working: s is dir? false
-
-//¯¯¯¯ not working: empty
-    rawPathStrFolder1.walk().forEach { println(it) }
-    folder2.walk().forEach { println(it) }
-    val f = File(rawPathStr)
-    val f2 = File(s2)
-    val f3 = File("g:").resolve(relative = sWithoutDocs + "docs")
-    println(f3.absoluteFile)
-    f3.walk().forEach { println(it) }
-//    File(sWithoutDocs).resolve(relative = "docs\\").walk().forEach  { println(it) }
-    File(rawPathStr).walk().forEach { println(it) }
-    File(s2).walk().forEach { println(it) }
-//    ;\____ not __ not working: empty
-
-    println("_".repeat(100))
-
-//¯¯¯¯ working examples ¯¯\
-//    File("G:\\My Web Sites\\https___autohotkey.com_docs_AutoHotkey.htm\\autohotkey.com\\docs\\").walk().forEach  { println(it) }
-//\____ working examples  __/
-
-
-//    File(s).walkTopDown().forEach { println(it) }
-//    File(replace).walkTopDown().forEach { println(it) }
-//    ;File(replace).walkTopDown().forEach { -> println(it) }
-// .replace('/', File.separatorChar);
-*/
+private fun doSomeTests() {
+    infoWindowsOS_File("""C:\""")
+    infoWindowsOS_File("""C:\Windows""")
+    infoWindowsOS_File("""G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\""")
 }
 
 //private fun String.replace(c: Char, s: String): Any {
