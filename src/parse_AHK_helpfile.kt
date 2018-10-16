f23cbf8c9d2c03dd2da4af09370a7e35464dfbed
Sebastian <sl5softwarelab@gmail.com>
2018-10-16 10:33:14 +0200

    dont work: list files of a Windows OS folder without using a double backslash file seperator

--- src/parse_AHK_helpfile.kt
+++ src/parse_AHK_helpfile.kt
@@ -1,31 +1,43 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    var s = """"G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\"""
-    println("s= " + s)
+    var rawPathStr = """"G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\docs\"""
+    println("rawPathStr= " + rawPathStr)
     var sWithoutDocs = """"G:\My Web Sites\https___autohotkey.com_docs_AutoHotkey.htm\autohotkey.com\"""
-                .replace("\\", "\\\\")
+            .replace("\\", "\\\\")
     println("sWithoutDocs=" + sWithoutDocs)
-    var s2 = s.replace("\\", "\\\\")
+    var s2 = rawPathStr.replace("\\", "\\\\")
     println("s2= " + s2)
 //    val replace = s.replace("\\", File.separator)
-    val inputFolder1 = File(s)
-    val inputFolder2 = File(s2)
-    inputFolder1.walk().forEach  { println(it) }
-    inputFolder2.walk().forEach  { println(it) }
-    val f = File(s)
+
+    val rawPathStrFolder1 = File(rawPathStr)
+    val folder2 = File(s2)
+//¯¯¯¯ not working: s is dir? false
+    println("s is dir? " + rawPathStrFolder1.isDirectory())
+    println("s2 is dir? " + folder2.isDirectory())
+//    s is dir? false
+//    s2 is dir? false
+//\____ not working: s is dir? false
+
+//¯¯¯¯ not working: empty
+    rawPathStrFolder1.walk().forEach { println(it) }
+    folder2.walk().forEach { println(it) }
+    val f = File(rawPathStr)
     val f2 = File(s2)
-    println("s is dir? " + f.isDirectory())
-    println("s2 is dir? " +f2.isDirectory())
     val f3 = File("g:").resolve(relative = sWithoutDocs + "docs")
     println(f3.absoluteFile)
-    f3.walk().forEach  { println(it) }
-
+    f3.walk().forEach { println(it) }
 //    File(sWithoutDocs).resolve(relative = "docs\\").walk().forEach  { println(it) }
-    File(s).walk().forEach  { println(it) }
-    File(s2).walk().forEach  { println(it) }
+    File(rawPathStr).walk().forEach { println(it) }
+    File(s2).walk().forEach { println(it) }
+//    ;\____ not __ not working: empty
+
     println("_".repeat(100))
+
+//¯¯¯¯ working examples ¯¯\
 //    File("G:\\My Web Sites\\https___autohotkey.com_docs_AutoHotkey.htm\\autohotkey.com\\docs\\").walk().forEach  { println(it) }
+//\____ working examples  __/
+
 
 //    File(s).walkTopDown().forEach { println(it) }
 //    File(replace).walkTopDown().forEach { println(it) }
